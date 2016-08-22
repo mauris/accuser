bcb104467b1928d213fad9eef0a6c53e4c6a6ac2
Sam-Mauris Yong <mauris@hotmail.sg>
2016-08-22 14:29:10 +0800

    added test for authenticate

--- test/testAccuser.js
+++ test/testAccuser.js
@@ -1,24 +1,35 @@
 var assert = require('assert');
 var sinon = require('sinon');
 var Accuser = require('../');
-var testAuth = require("./../testAuth.json");
 
 describe("Accuser", function() {
   var accuser;
-  var token = testAuth["token"];
 
   beforeEach(function() {
     accuser = new Accuser();
-    accuser.authenticate({
-      type: "oauth",
-      token: token
-    });
   });
 
-  it("should be able to add a new repository", function(next) {
+  it("should authenticate with Github", function(next) {
     var repository = accuser.addRepository("mauris", "accuser");
     assert(repository);
     assert(accuser.repos[0] === repository);
+    next();
+  });
+
+  it("should be able to add a new repository", function(next) {
+    accuser.github = {
+      authenticate: function(config){
+        assert(config.type === "oauth");
+        assert(config.token === "some token");
+      }
+    };
+    var mock = sinon.mock(accuser.github);
+    mock.expects("authenticate").once();
+    accuser.authenticate({
+      "type": "oauth",
+      "token": "some token"
+    });
+    mock.verify();
     next();
   });
 
@@ -34,50 +45,49 @@ describe("Accuser", function() {
         }
       }
     };
-      accuser.github = {
-        issues: {
-          addAssigneesToIssue: function(obj) {
-            assert(obj.repo === pr.base.repo.name);
-            assert(obj.user === pr.base.repo.owner.login);
-            assert(obj.number === pr.number);
-            assert(obj.assignees[0] === "mauris");
-          }
+    accuser.github = {
+      issues: {
+        addAssigneesToIssue: function(obj) {
+          assert(obj.repo === pr.base.repo.name);
+          assert(obj.user === pr.base.repo.owner.login);
+          assert(obj.number === pr.number);
+          assert(obj.assignees[0] === "mauris");
         }
-      };
-      var mock = sinon.mock(accuser.github.issues);
-      mock.expects("addAssigneesToIssue").once();
-      accuser.accuse(pr, ["mauris"]);
-      mock.verify();
-      next();
-    });
+      }
+    };
+    var mock = sinon.mock(accuser.github.issues);
+    mock.expects("addAssigneesToIssue").once();
+    accuser.accuse(pr, ["mauris"]);
+    mock.verify();
+    next();
+  });
 
-    it("should add a comment to a pull request", function(next) {
-      var pr = {
-        number: 20,
-        base: {
-          repo: {
-            name: "accuser",
-            owner: {
-              login: "mauris"
-            }
+  it("should add a comment to a pull request", function(next) {
+    var pr = {
+      number: 20,
+      base: {
+        repo: {
+          name: "accuser",
+          owner: {
+            login: "mauris"
           }
         }
-      };
-      accuser.github = {
-        issues: {
-          createComment: function(obj) {
-            assert(obj.repo === pr.base.repo.name);
-            assert(obj.user === pr.base.repo.owner.login);
-            assert(obj.number === pr.number);
-            assert(obj.body === "some comment");
-          }
+      }
+    };
+    accuser.github = {
+      issues: {
+        createComment: function(obj) {
+          assert(obj.repo === pr.base.repo.name);
+          assert(obj.user === pr.base.repo.owner.login);
+          assert(obj.number === pr.number);
+          assert(obj.body === "some comment");
         }
-      };
-      var mock = sinon.mock(accuser.github.issues);
-      mock.expects("createComment").once();
-      accuser.comment(pr, "some comment");
-      mock.verify();
-      next();
-    });
-
+      }
+    };
+    var mock = sinon.mock(accuser.github.issues);
+    mock.expects("createComment").once();
+    accuser.comment(pr, "some comment");
+    mock.verify();
+    next();
+  });
 });
