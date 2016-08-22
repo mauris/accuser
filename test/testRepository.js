3128dde86d8a9a4205ec924f2476c37afb33ef0a
Sam-Mauris Yong <mauris@hotmail.sg>
2016-08-22 14:58:21 +0800

    added more test cases for repository

--- test/testRepository.js
+++ test/testRepository.js
@@ -14,4 +14,22 @@ describe("Repository", function() {
     assert(repository.repo === "accuser");
     next();
   });
+
+  it("should create new worker using the newWorker() method", function(next){
+    assert(repository.workers.length === 0);
+    repository.newWorker();
+    assert(repository.workers.length === 1);
+    next();
+  });
+
+  it("should allow chaining filter() and do() methods for newWorker() method", function(next){
+    var chainer = repository.newWorker();
+    assert(repository.workers[0].filters.length === 0);
+    assert(repository.workers[0].do.length === 0);
+    assert(chainer === chainer.filter(function(){}));
+    assert(chainer === chainer.do(function(){}));
+    assert(repository.workers[0].filters.length === 1);
+    assert(repository.workers[0].do.length === 1);
+    next();
+  });
 });
